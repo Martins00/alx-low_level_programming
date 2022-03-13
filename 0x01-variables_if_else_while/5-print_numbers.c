2058af3e201edbf7ce3ece108f4d7b2f6d02de67
Olatunji Martins <martinsolatunji2@gmail.com>
2022-03-13 00:06:07 -0800

    New file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ *
+ *  * main - main block
+ *
+ *   * Description: print alphabet in lowercase
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char c = '0';
+
+
+
+			while (c <= '9')
+
+					{
+
+								putchar(c);
+
+										c++;
+
+											}
+
+
+
+				putchar('\n');
+
+					return (0);
+
+}
